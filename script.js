892e97e8f9f980d0a1ca8efcf3d9e3cbb3f951a5
SirYurko <55402601+SirYurko@users.noreply.github.com>
2024-11-24 17:47:08 +0200

    Object

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
-console.log("Hello World")
+// const basket = {
+//     Laptop: 1500,
+//     Keyboard: 100,
+//     HDMI: 10,
+// };
+
+// delete basket.HDMI;
+
+// // const goods = { HDMI: 10, Mouse: 25,};
+// // const order = { ...basket, ...goods};
+
+// const calculateTotal = function(obj) {
+//     let total = 0;
+//     for (const key in obj) {
+//         total += obj[key];
+//     }
+//     return total;
+// }
+
+// const mixTotal = (obj) => {
+//     obj.total = calculateTotal(obj);
+// };
+
+// mixTotal(basket);
+// calculateTotal(basket);
+// console.log(basket);
+
+
+// const user = {
+//     name: "Yurii",
+//     age: 22,
+//     email: "mitkevichy@gmail.com",
+//     getInfo: function() {
+//         return `Name: ${this.name}, Age: ${this.age}`;
+//     },
+//     updateEmail: function (editEmail) {
+//         this.email = editEmail;
+//     }
+// };
+
+// console.log(user.getInfo())
+// console.log(user.updateEmail("Test@gmail.com"));
+
+
+// --------------------
+// const book = {
+//     title: "DeadHeads",
+//     author: "Clark Harris",
+//     year: 1992,
+//     isAvailable: true,
+//     getBookInfo: function () {
+//         return `title: ${this.title}, author: ${this.author}, year: ${this.year}, isAvailable: ${this.isAvailable}`
+//     },
+//     changeAvailability: function (){
+//         if(this.isAvailable === true){
+//             this.isAvailable = false;
+//         } else {
+//             this.isAvailable = true;
+//         }
+//     }
+    
+// }
+// book.changeAvailability();
+// console.log(book.getBookInfo())
