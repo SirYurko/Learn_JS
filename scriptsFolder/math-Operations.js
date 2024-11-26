cccf9f9c95cc32ee5cf1e74810ce08052f0590e9
SirYurko <55402601+SirYurko@users.noreply.github.com>
2024-11-26 23:30:28 +0200

    base math. operations 2.0

--- scriptsFolder/math-Operations.js
+++ scriptsFolder/math-Operations.js
@@ -1,14 +1,103 @@
-"" + 1 + 0       //"10"
-"" - 1 + 0       //"10" -1
-true + false     //NaN  1
-6 / "3"         //NaN  2
-"2" * "3"       //NaN  6
-4 + 5 + "px"    //"10px"  9px
-"$" + 4 + 5      //"$45"
-"4" - 2         //NaN  2
-"4px" - 2       //NaN  
-"  -9  " + 5    //"-95"  " -9 5" 
-"  -9  " - 5    //NaN -14
-null + 1        //1  
-undefined + 1   //NaN
-" \t \n" - 2    //NaN -2
+// "" + 1 + 0       //"10"
+// "" - 1 + 0       //"10" -1
+// true + false     //NaN  1
+// 6 / "3"         //NaN  2
+// "2" * "3"       //NaN  6
+// 4 + 5 + "px"    //"10px"  9px
+// "$" + 4 + 5      //"$45"
+// "4" - 2         //NaN  2
+// "4px" - 2       //NaN  
+// "  -9  " + 5    //"-95"  " -9 5" 
+// "  -9  " - 5    //NaN -14
+// null + 1        //1  
+// undefined + 1   //NaN
+// " \t \n" - 2    //NaN -2
+
+//------------------------------
+// Задачки від GPT
+
+// let x = 5;
+// x = -x;
+// y = 2;
+// c = x - y;
+// console.log(c)
+// -------------------
+// let a = 8;
+// let b = 3;
+
+// let c = a * b; // Добуток
+// let d = a / b; // Частка
+// let e = a % b; // Остача
+// let s = a ** b; // Степінь
+
+// console.log("Добуток: " + c);
+// console.log("Частка: " + d.toFixed(3));
+// console.log("Остача: " + e);
+// console.log("Степінь: " + s);
+// -----------------------
+
+// let firstName = "John";
+// let lastName = "Doe";
+
+// console.log(firstName + " " + lastName)
+
+// --------------------------------------
+
+// let x = "25";
+// let y = 10;
+// x = +x;
+
+// c = x + y;
+
+// console.log(c)
+
+// ---------------------------------------
+//    // 5(50)   3(48)    2(16)      1(1)
+// let x = 2 + 3 * 4 ** 2 / (2 - 1);
+// let y = 5;
+
+// let c = x + y;
+// console.log(c)
+// --------------------------------------
+// let x = 10;
+
+// x = x + 5;
+
+// x = x * 2;
+
+// x = x / 3;
+
+// console.log(x)
+// -----------------------------------
+// let counter = 5;
+
+// console.log(++counter);
+// console.log(counter--);
+// console.log(counter)
+// -----------------------------------
+// let apples = "4";
+// let oranges = "6";
+// let result = +apples + +oranges;
+// console.log(result)
+// -----------------------------------
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let result = a + b * (c - a) ** b / c;
+
+// console.log(result)
+// ------------------------------------
+// let x;
+// let y;
+// let z;
+
+// x = y = z = 15 + 5;
+
+// console.log(x, y, z)
+// ------------------------------------
+// let price = "199.99";
+// let quantity = "3";
+
+// let result = +price + +quantity;
+
+// console.log(result)
