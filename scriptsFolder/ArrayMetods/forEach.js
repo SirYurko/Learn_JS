386fa591bad5c8a6a10a30183d921615c5affbfd
SirYurko <55402601+SirYurko@users.noreply.github.com>
2024-11-28 23:00:05 +0200

    forEach "S"

--- scriptsFolder/ArrayMetods/forEach.js
+++ scriptsFolder/ArrayMetods/forEach.js
@@ -17,11 +17,15 @@ function arrayForEachOne(){
 }
 
 // -------------------------------------------
-
-// ДОПРАКТИКУВАТИ
-// function arrayForEachTwo(){
-    unitedMonth.forEach(function(month, index){
-        console.log(` ${index + 1} ${month}, `)
+// створення нового масива
+// який містить в собі Місяці які починаються з "S"
+function arrayForEachTwo(){
+    let unitedMonthCopy = [];                   // оголошення змінної для зберігання місяців
+    unitedMonth.forEach(function(month){        // викликаємо метод forEach>створюємо функцію для кожного місяця з масива
+        if(month.startsWith("S")){              // якщо перше буква починається з "S"      
+            console.log(month);                 // виводить це слово в консоль
+            unitedMonthCopy.push(month);        // та добавляє в змінну яку створили раніше
+        }
     })
-
-
+console.log(unitedMonthCopy)                    // виводимо масив в консоль
+}
