c8264a08f5eacf5e9cd31db0ed871aa9c925fb41
SirYurko <55402601+SirYurko@users.noreply.github.com>
2024-11-29 10:41:53 +0200

    add forEach task object in array

--- scriptsFolder/ArrayMetods/forEach.js
+++ scriptsFolder/ArrayMetods/forEach.js
@@ -5,7 +5,6 @@ import  unitedMonth  from './array.js';
 // months.forEach(function(month, index) {
 //     console.log(`Місяць ${index + 1}: ${month}`);
 //   });
-console.log(unitedMonth);
 function arrayForEachOne(){
     let totalLength = 0; // створюємо змінну в яку будемо записувати кількість символів в місяцях !за межами функції!
     unitedMonth.forEach( function (month, index){   // викликаємо метод forEach>створюємо функцію для кожного місяця з масива та індекс
@@ -29,3 +28,18 @@ function arrayForEachTwo(){
     })
 console.log(unitedMonthCopy)                    // виводимо масив в консоль
 }
+// --------------------------------------------------------------------------------
+// Задача, об'єкт в масиві, потрібно взнати загальну вартість продуктів
+const products = [              // маємо масив об'єктів
+    { name: "Apple", price: 15 },
+    { name: "Banana", price: 10 },
+    { name: "Cherry", price: 20 },
+    { name: "Date", price: 25 }
+];
+let totalPrice = 0;                     //створюємо змінну в яку будемо додавати ціни
+products.forEach(function(product){     // викликаємо метод forEact і в функції зазначаємо product кожен елемент масиву
+    console.log(`Product: ${product.name}, Price: ${product.price}`) // виводимо ім'я та ціну
+    totalPrice += product.price;   // додаємо до змінної ціну кожної позиції
+})
+
+console.log("Загальна сума: " + totalPrice) // виводимо в консоль
